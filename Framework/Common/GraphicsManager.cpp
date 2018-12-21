ac43e4b838fc628551ec9ebeb68520fac9895f1b
HanxRen <969765303@qq.com>
2018-12-21 11:48:10 -0500

    finished content in article 6

---
+++ Framework/Common/GraphicsManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hanxiang Ren on 2018-12-21.
+//
+
+#include "GraphicsManager.hpp"
---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Hanxiang Ren on 2018-12-21.
+//
+
+#pragma once
+
+#include "IRuntimeModule.hpp"
+
+namespace My {
+    class GraphicsManager : public IRuntimeModule {
+    public:
+        virtual ~GraphicsManager() {}
+    };
+
+}
+
+
